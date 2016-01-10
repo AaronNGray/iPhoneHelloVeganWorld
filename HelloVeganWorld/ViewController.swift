520e1101428b4da4845b9141986022322e2b2fed
Aaron Gray <aarongray@aarons-macbook-pro.local>
2016-01-10 13:29:17 +0000

    Initial Commit

---
+++ HelloVeganWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloVeganWorld
+//
+//  Created by Aaron Gray on 10/01/2016.
+//  Copyright © 2016 Aaron Gray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
